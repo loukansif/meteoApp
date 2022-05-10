d3835a3e657eaa770d53348f17628338b83afcff
slami <lamislimane@gmail.com>
2022-05-10 16:57:05 +0200

    add bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 function myFunction() {
   const CAGEDATA_API_KEY = "c51703746f42417aad34698a35135e7b";
   const WEATHERMAP_API_KEY = "26f95dc23fad5b112f7c3fbf3a8a102c";
-  let ladate = new Date();
+  let hourUs = new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric', second: 'numeric' });
+  let ajd = new Date();
   let week = [
     "Sunday",
     "Monday",
@@ -11,7 +12,7 @@ function myFunction() {
     "Friday",
     "Saturday",
   ];
-  let indexDay = ladate.getDay();
+  let indexDay = ajd.getDay();
   const city_name = document.getElementById("city").value;
   const CAGEDATA_URL = `https://api.opencagedata.com/geocode/v1/json?q=${city_name}&key=${CAGEDATA_API_KEY}&language=fr&pretty=1`;
   let imgSrc = "";
@@ -31,13 +32,39 @@ function myFunction() {
       const longitude = data.results[0].geometry.lng;
       console.log(`La latitude de ${city_name} est ${latitude}`);
       console.log(`La longitude de ${city_name} est ${longitude}`);
-
+      const DAY_URL = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`;
       const WEATHERMAP_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHERMAP_API_KEY}`;
       fetch(WEATHERMAP_URL)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
+          fetch(DAY_URL)
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+                console.log(data)
+              console.log(data.results.sunset);
+              console.log(hourUs)
+              console.log(hourUs - data.results.sunset)
+
+              if(hourUs >= data.results.sunset){
+                  console.log("ok")
+                  document.body.style.background = '';
+                  document.body.style.background = 'linear-gradient(#01091F, #3776FB)';
+                  document.body.style.color = 'white';
+                }else {
+                    console.log("noOk")
+                    document.body.style.background = '';
+                    document.body.style.backgroundColor = '#19c8e3';
+                    document.body.style.color = 'black';
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+
           forecasts.innerHTML = "";
           console.log(data);
           console.log(week[indexDay]);
@@ -60,16 +87,16 @@ function myFunction() {
               imgSrc = "./assets/images/rain.svg";
             }
 
-            let d = document.createElement("div");
-            d.classList = "forecast";
-            let h = document.createElement("H3");
-            let t = document.createTextNode(day);
-            let img = document.createElement("img");
-            img.src = imgSrc;
-            h.appendChild(t);
-            d.appendChild(h);
-            d.appendChild(img);
-            document.getElementById("forecasts").appendChild(d);
+            let addDiv = document.createElement("div");
+            addDiv.classList = "forecast";
+            let addH3 = document.createElement("H3");
+            let textH3 = document.createTextNode(day);
+            let addImg = document.createElement("img");
+            addImg.src = imgSrc;
+            addH3.appendChild(textH3);
+            addDiv.appendChild(addH3);
+            addDiv.appendChild(addImg);
+            document.getElementById("forecasts").appendChild(addDiv);
             if (indexDay + 1 == 7) {
               indexDay = 0;
             } else {
